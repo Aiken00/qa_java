2d50533df3943fefbc3464c8c5fa1a752c4c740b
Aiken <monckeycpofficial@gmail.com>
2024-07-08 17:47:07 +0600

    'trying_to_make_pull_request'

--- src/main/java/com/example/Animal.java
+++ src/main/java/com/example/Animal.java
@@ -1,12 +1,11 @@
 package com.example;
 
 import java.util.List;
-// Комментарий
 public class Animal {
 
     public List<String> getFood(String animalKind) throws Exception {
         if ("Травоядное".equals(animalKind)) {
-            return List.of("Трава", "Различные растени");
+            return List.of("Трава", "Различные растения");
         } else if ("Хищник".equals(animalKind)) {
             return List.of("Животные", "Птицы", "Рыба");
         } else {
